66053e912116c9c0932f8fb8145cf07ac45250b8
David Small <dave@dsmall.co.uk>
2020-10-19 21:26:22 +0100

    fix: Add Dynamic setting of the package version in the Springboard365.StyleCop.props file.

--- Build/NugetPush.bat
+++ Build/NugetPush.bat
@@ -1,10 +1,16 @@
 @echo off
-SET packageVersion=1.0.0
+SET packageVersion=1.0.2
+SET oldPackageVersion=1.0.0
+SET propsFileName=Springboard365.StyleCop.props
 SET id=Springboard365.StyleCop
 SET author="Springboard 365 Ltd"
 SET repo="https://github.com/SpringBoard365/Springboard365.StyleCop"
 SET description="StyleCop helper package from Springboard 365 Ltd ."
 SET tags="StyleCop DotNetAnalyzers Roslyn Diagnostic Analyzer CleanCode"
+
+cscript replace.vbs %propsFileName% %oldPackageVersion% %packageVersion%
+cscript //NoLogo replace.vbs %propsFileName% %oldPackageVersion% %packageVersion%
+echo Changed package version from %oldPackageVersion% to %packageVersion%
 
 NuGet.exe pack Springboard365.StyleCop.nuspec -Build -symbols -Version %packageVersion% -Properties Configuration=Release;id=%id%;author=%author%;repo=%repo%;description=%description%;tags=%tags%;
 
