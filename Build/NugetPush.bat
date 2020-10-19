e66643d0265fb3fcd9bf95f851656972453df6a8
David Small <dave@dsmall.co.uk>
2020-10-19 21:43:39 +0100

    Add git cleanup task so that oldPackageVersion does not need changed.

--- Build/NugetPush.bat
+++ Build/NugetPush.bat
@@ -8,11 +8,18 @@ SET repo="https://github.com/SpringBoard365/Springboard365.StyleCop"
 SET description="StyleCop helper package from Springboard 365 Ltd ."
 SET tags="StyleCop DotNetAnalyzers Roslyn Diagnostic Analyzer CleanCode"
 
+echo Changing package version from %oldPackageVersion% to %packageVersion%
 cscript //NoLogo replace.vbs %propsFileName% %oldPackageVersion% %packageVersion%
-echo Changed package version from %oldPackageVersion% to %packageVersion%
 
+echo Packing
 NuGet.exe pack Springboard365.StyleCop.nuspec -Build -symbols -Version %packageVersion% -Properties Configuration=Release;id=%id%;author=%author%;repo=%repo%;description=%description%;tags=%tags%;
 
+echo Pushing
 NuGet.exe push Springboard365.StyleCop.%packageVersion%.nupkg -Source "https://api.nuget.org/v3/index.json"
+
+pause
+
+echo Undoing changes to the props file
+git checkout -- Springboard365.StyleCop.props
 
 pause
